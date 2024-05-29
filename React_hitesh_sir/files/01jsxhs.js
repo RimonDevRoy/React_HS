44114b788ae8ac6392d0c06af965e975cd223108
Rimon Dev Roy <rimoncode.all@gmail.com>
2024-05-29 23:42:48 +0600

    01jsxhs.js completed

---
+++ React_hitesh_sir/files/01jsxhs.js
@@ -0,0 +1,96 @@
+/*
+// REACT ROADMAP
+
+● React.js makes managing and building complex frontend projects easy.
+
+● React has high job market but never be a React developer instead be a JS developer(core software developer).
+
+● It will be overkill if we use React to build small and easy projects. Because React compiles to HTML, CSS and JS at the end and this takes time.
+
+● One example of complex project is:
+
+A button click is making changes in several places instead of one.
+
+● There was a problem in facebook that when somebody messaged us in messanger, the message count did not use to disappear even after we were done with reading them. It was called "Phantom message problem". This rose a necessity to develop React.js in order to solve it.
+
+● State(can be loosely said variables) is controlled by JS.
+
+● UI is managed by DOM because DOM manipulation arises UI interaction.
+
+● State and UI should be able to interact with each other, should be in sync with each other.
+
+● Babel, fibre, Virtual DOM, diff algorithm, hydration, tree shaking are in-depth concepts of React.js
+
+● React.js is a library.
+
+● Frameworks are very strict for their rules, but Libraries are no strict for their rules, that means if some error is caused by the user, it is forgiven.
+
+● Topics to learn:
+
+1. Core of React.js (state or UI manipulation, JSX)
+
+2. Component reusability.
+
+3. Reusing of components (props)
+
+4. How to propagate changes (hooks)
+
+● Changing a variable's value does not by own manipulate the UI. We have to make it do that.
+
+● JSX: It basically we are writing HTML in JS.
+
+● Component reusability: How components can be made once and used in multiple places and use cases.
+
+● We can pass props into components.
+
+● The changes made in variables are propagated by the help of hooks. Example: useEffect, useMemo etc.
+
+● React.js makes SPA(Single Page Application). SPA means page never reloads. Router adds the page reloading ability to React applications. React-Router-DOM is an extra new package(library) that enables this and it is not a part of core React.
+
+● Additional Add-ons to React:
+
+1. Router
+
+2. State management (React does not have it by default) -> Redux, Redux toolkit, zustand, context API
+
+Redux toolkit is an easier extension of Redux.
+
+Context API gives hooks that helps in state management.
+
+3. Class based component -> legacy code
+
+4. BAAS Apps -> social media clone, e-commerce app
+
+● As in older versions of React class based components are still in maintainence in many companies, we can learn it, but this is not good for career growth. Some components like deadMount, unMount etc. were taken from Swift / iOS.s
+
+● BAAS: Backend As A Service.
+Appwrite, Firebase, Superbase etc. some of the BAAS. BAAS offer backend by themselves, we do not need to write backend rather just use them and do work in Frontend.
+
+● After React.js:
+
+1. Shift to Backend
+
+2. Shift to Next.js
+
+3. 
+
+● Code generated from React.js is not optimized because it has no SEO.
+
+● The code of React only ships JS into browser, and broswer later generates HTML, CSS from the code. This arises an issue in browser rendering. As a result, the page loads slowly although small applications appear to be loading very fast.
+
+● React.js has not routing by default.
+
+● Next.js is made based on React.js
+
+● Next.js allows us to write both frontend and backend.
+
+● Next.js is a framework.
+
+● 
+
+● 
+
+● 
+
+*/
+
