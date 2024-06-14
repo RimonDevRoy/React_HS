8c8afeb94050af204689800ea9215719e2d8343a
Rimon Dev Roy <rimoncode.all@gmail.com>
2024-06-14 15:00:13 +0600

    update after 09jsxhs.js - 2

--- React_hitesh_sir/files/09jsxhs.js
+++ React_hitesh_sir/files/09jsxhs.js
@@ -1,9 +1,13 @@
 /*
 // BUILDING A REACT PROJECT - BGCHANGER
 
-● 
+● To write inline CSS in JSX, we need to write styles in:
 
-● 
+style={{styles}}
+
+In this condition we don't need to give another {} to write a variable. Just simply write the variable name.
+
+● onClick expects a callback.
 
 ● 
 
