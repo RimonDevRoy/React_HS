4524d3c3a3ca2ebc2abfe39b9a8d4f056fb9a3f8
Rimon Dev Roy <rimoncode.all@gmail.com>
2024-06-27 17:01:38 +0600

    add updateTodo, isTodoEditableToggler and other functionalities

--- 15reduxToolkitTodo/src/components/AddTodo.jsx
+++ 15reduxToolkitTodo/src/components/AddTodo.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { addTodo } from "../features/todo/todoSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { addTodo, updateTodo, isTodoEditableToggler } from "../features/todo/todoSlice";
 
 function AddTodo() {
   const [input, setInput] = useState("");
+  const [editInput, setEditInput] = useState("");
   const dispatch = useDispatch();
+  const todoIds = useSelector(state => state.todoIds)
+  const isAnyTodoEditable = useSelector(state => state.isAnyTodoEditable)
 
   const addTodoHandler = (e) => {
     e.preventDefault();
@@ -12,20 +15,33 @@ function AddTodo() {
     setInput("");
   };
 
+  const EditTodoHandler = (e) => {
+    e.preventDefault()
+    // dispatch(updateTodo({id: ((useSelector(state => state.todoIds))[0]), text: editInput}))
+    // dispatch(updateTodo({id: (useSelector(state => state.todoIds))[0], text: editInput}))
+    dispatch(updateTodo({id: todoIds[0], text: editInput}))
+    setEditInput("")
+    // dispatch(isTodoEditableToggler(((useSelector(state => state.todoIds))[0])))
+    // dispatch(isTodoEditableToggler((useSelector(state => state.todoIds))[0]))
+    dispatch(isTodoEditableToggler(todoIds[0]))
+
+    /* Didn't understand why the above 4 lines did not work */
+  }
+
   return (
-    <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
+    <form onSubmit={isAnyTodoEditable ? EditTodoHandler : addTodoHandler} className="space-x-3 mt-12">
       <input
         type="text"
         className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-        placeholder="Enter a Todo..."
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
+        placeholder={isAnyTodoEditable ? "Edit here..." : "Enter a Todo..."}
+        value={isAnyTodoEditable ? editInput : input}
+        onChange={isAnyTodoEditable ? (e) => setEditInput(e.target.value) : (e) => setInput(e.target.value)}
       />
       <button
         type="submit"
         className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
       >
-        Add Todo
+        {isAnyTodoEditable ? "Update" : "Add Todo"}
       </button>
     </form>
   );
--- 15reduxToolkitTodo/src/features/todo/todoSlice.js
+++ 15reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -1,7 +1,10 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { useEffect } from "react";
 
 const initialState = {
-  todos: [{ id: 1, text: "Hello world" }],
+  todos: [{ id: nanoid(), text: "Hello world", isTodoEditable: false }],
+  todoIds: [],
+  isAnyTodoEditable: false
   // This will be present at the time of initial render.
 };
 
@@ -9,10 +12,12 @@ export const todoSlice = createSlice({
   name: "todo",
   initialState,
   reducers: {
+    
     addTodo: (state, action) => {
       const todo = {
         id: nanoid(),
         text: action.payload,
+        isTodoEditable: false
       };
       state.todos.push(todo);
     },
@@ -26,10 +31,54 @@ export const todoSlice = createSlice({
     removeTodo: (state, action) => {
       state.todos = state.todos.filter((todo) => todo.id !== action.payload);
     },
+
+    isTodoEditableToggler: (state, action) => {
+
+      if (!(state.todoIds.includes(action.payload))) {
+        state.todoIds.push(action.payload)
+      } else if (state.todoIds.includes(action.payload)) {
+        state.todoIds = state.todoIds.filter((todoId) => todoId !== action.payload)
+      }
+
+      state.todos.forEach(todo => {
+        if (todo.id === action.payload) todo.isTodoEditable = !todo.isTodoEditable
+      });
+        
+      // let isEditable = state.todos.filter((todo) => todo.isTodoEditable === true)
+
+      // let isNotEditable = state.todos.filter((todo) => todo.isTodoEditable === false)
+
+      // if (isEditable.length >= 1) state.isAnyTodoEditable = true
+      // if (isNotEditable.length >= 1) state.isAnyTodoEditable = false
+
+      /*
+      The above code of 'isAnyTodoEditable' check has made me realize a big lesson!!!
+      The lesson is: Always visualize every line of code you are writing in a notepaper with diagrams of the actions that the code will execute. Such as: here, 'isEditable' and 'isNotEditable' will co-exist and will interfere in actual intention od code with their 'length'.
+      Here, even if a single todo was editable, it was increasing the length of 'isEditable', but the rest of the non-editable todos were also making up the length of 'isNotEditable'. Thus, at the end 'isAnyTodoEditable' was assigned false until all the todos were editable.
+      You should try the code below with either only 'isEditable' or 'isNotEditable'.
+      */
+
+      // let isEditable = state.todos.filter((todo) => todo.isTodoEditable === true)
+
+      // if (isEditable.length >= 1) {
+      //   state.isAnyTodoEditable = true
+      // } else if (isEditable.length === 0){
+      //   state.isAnyTodoEditable = false
+      // }
+
+      // or for better optimization,
+
+      if ((state.todos.filter((todo) => todo.isTodoEditable === true)).length >= 1) {
+        state.isAnyTodoEditable = true
+      } else {
+        state.isAnyTodoEditable = false
+      }
+    },
+
   },
 });
 
-export const { addTodo, updateTodo, removeTodo } = todoSlice.actions;
+export const { addTodo, updateTodo, removeTodo, isTodoEditableToggler } = todoSlice.actions;
 
 export default todoSlice.reducer;
 // It could be that 'default' exports can be renamed as wish while importing in other files. In store.js, the above default export is imported as 'todoReducer'. 
